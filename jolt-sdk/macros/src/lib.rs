26c70f8a90e9e5ca1fa908f5dada7a513aab8aa0
Ethan Ng <thien09062000@gmail.com>
2025-03-12 21:05:17 +0700

    fix

--- jolt-sdk/macros/src/lib.rs
+++ jolt-sdk/macros/src/lib.rs
@@ -443,10 +443,6 @@ impl MacroBuilder {
                 memory_init: Vec<(u64, u8)>,
             }
 
-            #[cfg(all(target_arch = "wasm32", not(feature = "guest")))]
-            use jolt::{JoltHyperKZGProof, MemoryLayout};
-
-
             #[cfg(target_arch = "wasm32")]
             fn deserialize_from_bin<'a, T: Deserialize<'a>>(
                 data: &'a [u8],
@@ -552,6 +548,7 @@ impl MacroBuilder {
             #[wasm_bindgen]
             #[cfg(all(target_arch = "wasm32", not(feature = "guest")))]
             pub fn #verify_wasm_fn_name(preprocessing_data: &[u8], proof_bytes: &[u8]) -> bool {
+                use jolt::{JoltHyperKZGProof, Serializable, MemoryLayout, RV32IJoltVM, ProofTranscript};
                 let decoded_preprocessing_data: DecodedData = deserialize_from_bin(preprocessing_data).unwrap();
                 let proof = JoltHyperKZGProof::deserialize_from_bytes(proof_bytes).unwrap();
                 let memory_layout = MemoryLayout::new(#max_input_size, #max_output_size);
