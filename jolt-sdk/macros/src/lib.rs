a5af314fa8a9441b8d11616c411ea0dd772a4011
Ethan Ng <thien09062000@gmail.com>
2025-03-15 20:17:51 +0700

    fix

--- jolt-sdk/macros/src/lib.rs
+++ jolt-sdk/macros/src/lib.rs
@@ -549,14 +549,20 @@ impl MacroBuilder {
             #[cfg(all(target_arch = "wasm32", not(feature = "guest")))]
             pub fn #verify_wasm_fn_name(preprocessing_data: &[u8], proof_bytes: &[u8]) -> bool {
                 use jolt::{Jolt, JoltHyperKZGProof, Serializable, MemoryLayout, RV32IJoltVM, ProofTranscript};
-                println!("start verify");
                 let decoded_preprocessing_data: DecodedData = deserialize_from_bin(preprocessing_data).unwrap();
                 let proof = JoltHyperKZGProof::deserialize_from_bytes(proof_bytes).unwrap();
-                println!("deserialilze done");
                 let memory_layout = MemoryLayout::new(#max_input_size, #max_output_size);
-                println!("memory_layout done {}", #max_input_size);
-                
+                let preprocessing = RV32IJoltVM::preprocess(
+                    decoded_preprocessing_data.bytecode,
+                    memory_layout,
+                    decoded_preprocessing_data.memory_init,
+                    1 << 20,
+                    1 << 20,
+                    1 << 24,
+                );
                 true
+                // let result = RV32IJoltVM::verify(preprocessing, proof.proof, proof.commitments, None);
+                // result.is_ok()
             }
         }
     }
